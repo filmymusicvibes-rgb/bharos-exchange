f4c21bf6d40d4677e64aa0bb5f7c864cdea81ada
Suresh Arika <suresharika@Sureshs-Mini.lan>
2026-03-28 18:41:57 +0530

    Replace Update1 with Update3 image for slide 1, remove large GIF

--- src/components/AnnouncementsSlider.tsx
+++ src/components/AnnouncementsSlider.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 
-import update1Img from '../assets/Update1.gif'
+import update1Img from '../assets/Update3.jpeg'
 import update2Img from '../assets/Update2.jpeg'
 import update3Img from '../assets/Update3.jpeg'
 
